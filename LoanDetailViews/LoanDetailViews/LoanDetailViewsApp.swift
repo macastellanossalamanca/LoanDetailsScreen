a3b0bc324c1e664e1129ab7cba754a78ccb50626
macastellanossalamanca <macastellanossalamanca@gmail.com>
2024-03-19 18:22:54 -0500

    Base Project

---
+++ LoanDetailViews/LoanDetailViews/LoanDetailViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoanDetailViewsApp.swift
+//  LoanDetailViews
+//
+//  Created by Sandra Salamanca on 19/03/24.
+//
+
+import SwiftUI
+
+@main
+struct LoanDetailViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
